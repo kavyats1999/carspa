c79e62a499345842290730bdf28e67af2491ab8e
kavya <tskavyasajeev@gmail.com>
2024-11-30 09:18:42 +0530

    carspa

---
+++ assets/js/custom.js
@@ -0,0 +1,341 @@
+(function($) {
+    'use strict';
+
+    // Mean Menu
+    $('.mean-menu').meanmenu({
+        meanScreenWidth: "991"
+    });
+
+    // Sticky, Go To Top JS
+    $(window).on('scroll', function() {
+        // Header Sticky JS
+        if ($(this).scrollTop() > 150) {
+            $('.navbar-area-style3').addClass("is-sticky");
+        } else {
+            $('.navbar-area-style3').removeClass("is-sticky");
+        };
+    });
+
+    // Sticky, Go To Top JS
+    $(window).on('scroll', function() {
+        // Header Sticky JS
+        if ($(this).scrollTop() > 150) {
+            $('.navbar-area').addClass("is-sticky");
+        } else {
+            $('.navbar-area').removeClass("is-sticky");
+        };
+    });
+
+    // Others Option For Responsive JS 
+    $(".others-option-for-responsive .dot-menu").on("click", function() {
+        $(".others-option-for-responsive .container .container").toggleClass("active");
+    });
+
+    // Preloader
+    $(window).on('loader', function() {
+        $('.preloader').fadeOut();
+    });
+    $(".loader").delay(1000).fadeOut("slow");
+    $("#overlayer").delay(1000).fadeOut("slow");
+
+    // Odometer JS
+    $('.odometer').appear(function(e) {
+        var odo = $(".odometer");
+        odo.each(function() {
+            var countNumber = $(this).attr("data-count");
+            $(this).html(countNumber);
+        });
+    });
+
+    // Popup JS
+    $(document).ready(function() {
+        $('.popup-youtube, .popup-vimeo, .popup-gmaps').magnificPopup({
+            disableOn: 700,
+            type: 'iframe',
+            mainClass: 'mfp-fade',
+            removalDelay: 160,
+            preloader: false,
+            fixedContentPos: false
+        });
+    });
+
+    // Service slider
+    $('.service-slider').owlCarousel({
+        loop: true,
+        margin: 20,
+        nav: true,
+        dots: true,
+        navText: [
+            '<i class="flaticon-back"></i>',
+            '<i class="flaticon-next"></i>'
+        ],
+        responsive: {
+            0: {
+                items: 1
+            },
+            576: {
+                items: 2
+            },
+            768: {
+                items: 2
+            },
+            992: {
+                items: 3
+            },
+            1200: {
+                items: 3
+            }
+        }
+    })
+
+    // Testimonial slider
+    $('.testimonial-slider').owlCarousel({
+        loop: true,
+        margin: 30,
+        nav: true,
+        dots: true,
+        navText: [
+            '<i class="flaticon-back"></i>',
+            '<i class="flaticon-next"></i>'
+        ],
+        responsive: {
+            0: {
+                items: 1
+            },
+            576: {
+                items: 1
+            },
+            768: {
+                items: 1
+            },
+            992: {
+                items: 2
+            },
+            1200: {
+                items: 2
+            }
+        }
+    })
+
+    // Clients slider
+    $('.clients-slider').owlCarousel({
+        loop: true,
+        margin: 0,
+        nav: false,
+        dots: true,
+        responsive: {
+            0: {
+                items: 1
+            },
+            576: {
+                items: 1
+            },
+            768: {
+                items: 1
+            },
+            992: {
+                items: 1
+            },
+            1200: {
+                items: 1
+            }
+        }
+    })
+
+    // Blog slider
+    $('.blog-slider').owlCarousel({
+        loop: true,
+        margin: 20,
+        nav: true,
+        dots: false,
+        navText: [
+            '<i class="flaticon-back"></i>',
+            '<i class="flaticon-next"></i>'
+        ],
+        responsive: {
+            0: {
+                items: 1
+            },
+            576: {
+                items: 1
+            },
+            768: {
+                items: 2
+            },
+            992: {
+                items: 3
+            },
+            1200: {
+                items: 3
+            }
+        }
+    })
+
+    // Banner Slider
+    $('.hero-slider').owlCarousel({
+        loop: true,
+        margin: 0,
+        nav: true,
+        items: 1,
+        dots: false,
+        navText: [
+            '<i class="flaticon-back"></i>',
+            '<i class="flaticon-next"></i>'
+        ],
+        responsive: {
+            0: {
+                items: 1
+            },
+            576: {
+                items: 1
+            },
+            768: {
+                items: 1
+            },
+            992: {
+                items: 1
+            },
+            1200: {
+                items: 1
+            }
+        }
+    });
+
+    // Before & After
+    $('.ba-slider').beforeAfter();
+
+    // Search Popup JS
+    $('.close-btn').on('click', function() {
+        $('.search-overlay').fadeOut();
+        $('.search-btn').show();
+        $('.close-btn').removeClass('active');
+    });
+    $('.search-btn').on('click', function() {
+        $(this).hide();
+        $('.search-overlay').fadeIn();
+        $('.close-btn').addClass('active');
+    });
+
+    // Datepicker
+    $("#datepicker1").datepicker();
+    $("#datepicker2").datepicker();
+
+    // Subscribe form JS
+    $(".newsletter-form").validator().on("submit", function(event) {
+        if (event.isDefaultPrevented()) {
+            // handle the invalid form...
+            formErrorSub();
+            submitMSGSub(false, "Please enter your email correctly.");
+        } else {
+            // everything looks good!
+            event.preventDefault();
+        }
+    });
+
+    function callbackFunction(resp) {
+        if (resp.result === "success") {
+            formSuccessSub();
+        } else {
+            formErrorSub();
+        }
+    }
+
+    function formSuccessSub() {
+        $(".newsletter-form")[0].reset();
+        submitMSGSub(true, "Thank you for subscribing!");
+        setTimeout(function() {
+            $("#validator-newsletter, #validator-newsletter-2").addClass('hide');
+        }, 4000)
+    }
+
+    function formErrorSub() {
+        $(".newsletter-form").addClass("animated shake");
+        setTimeout(function() {
+            $(".newsletter-form").removeClass("animated shake");
+        }, 1000)
+    }
+
+    function submitMSGSub(valid, msg) {
+        if (valid) {
+            var msgClasses = "validation-success";
+        } else {
+            var msgClasses = "validation-danger";
+        }
+        $("#validator-newsletter, #validator-newsletter-2").removeClass().addClass(msgClasses).text(msg);
+    }
+
+    // Input Plus & Minus Number JS
+    $('.input-counter').each(function() {
+        var spinner = jQuery(this),
+            input = spinner.find('input[type="text"]'),
+            btnUp = spinner.find('.plus-btn'),
+            btnDown = spinner.find('.minus-btn'),
+            min = input.attr('min'),
+            max = input.attr('max');
+
+        btnUp.on('click', function() {
+            var oldValue = parseFloat(input.val());
+            if (oldValue >= max) {
+                var newVal = oldValue;
+            } else {
+                var newVal = oldValue + 1;
+            }
+            spinner.find("input").val(newVal);
+            spinner.find("input").trigger("change");
+        });
+        btnDown.on('click', function() {
+            var oldValue = parseFloat(input.val());
+            if (oldValue <= min) {
+                var newVal = oldValue;
+            } else {
+                var newVal = oldValue = 1;
+            }
+            spinner.find("input").val(newVal);
+            spinner.find("input").trigger("change");
+        });
+    });
+
+    // Count Time JS
+    function makeTimer() {
+        var endTime = new Date("November 30, 2025 17:00:00 PDT");
+        var endTime = (Date.parse(endTime)) / 1000;
+        var now = new Date();
+        var now = (Date.parse(now) / 1000);
+        var timeLeft = endTime - now;
+        var days = Math.floor(timeLeft / 86400);
+        var hours = Math.floor((timeLeft - (days * 86400)) / 3600);
+        var minutes = Math.floor((timeLeft - (days * 86400) - (hours * 3600)) / 60);
+        var seconds = Math.floor((timeLeft - (days * 86400) - (hours * 3600) - (minutes * 60)));
+        if (hours < "10") {
+            hours = "0" + hours;
+        }
+        if (minutes < "10") {
+            minutes = "0" + minutes;
+        }
+        if (seconds < "10") {
+            seconds = "0" + seconds;
+        }
+        $("#days").html(days + "<span>Days</span>");
+        $("#hours").html(hours + "<span>Hours</span>");
+        $("#minutes").html(minutes + "<span>Minutes</span>");
+        $("#seconds").html(seconds + "<span>Seconds</span>");
+    }
+    setInterval(function() {
+        makeTimer();
+    }, 300);
+
+
+    // Scroll Event
+    $(window).on('scroll', function() {
+        var scrolled = $(window).scrollTop();
+        if (scrolled > 600) $('.go-top').addClass('active');
+        if (scrolled < 600) $('.go-top').removeClass('active');
+    });
+    // Click Event
+    $('.go-top').on('click', function() {
+        $("html, body").animate({
+            scrollTop: "0"
+        }, 0);
+    });
+
+})(jQuery);
